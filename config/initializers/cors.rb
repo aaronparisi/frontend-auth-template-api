cecf196a831bed046a033fc29c5b1dfa1bf00001
Aaron <parisi.aaron@gmail.com>
2021-02-27 00:59:44 +0000

    added session stuff

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,15 +1,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:8080', 
-      'http://localhost:5000', 
-      'https://www.frontend-auth-ca586.firebaseapp.com', 
-      'http://www.frontend-auth-ca586.firebaseapp.com', 
-      'https://frontend-auth-ca586.firebaseapp.com', 
-      'http://frontend-auth-ca586.firebaseapp.com', 
-      'http://www.frontend-auth.aaronparisidev.com', 
-      'https://www.frontend-auth.aaronparisidev.com', 
-      'http://frontend-auth.aaronparisidev.com',
-      'https://frontend-auth.aaronparisidev.com'
+      'http://localhost:5000'
+      # 'https://www.frontend-auth-ca586.firebaseapp.com', 
+      # 'http://www.frontend-auth-ca586.firebaseapp.com', 
+      # 'https://frontend-auth-ca586.firebaseapp.com', 
+      # 'http://frontend-auth-ca586.firebaseapp.com', 
+      # 'http://www.frontend-auth.aaronparisidev.com', 
+      # 'https://www.frontend-auth.aaronparisidev.com', 
+      # 'http://frontend-auth.aaronparisidev.com',
+      # 'https://frontend-auth.aaronparisidev.com'
 
     resource '*',
       headers: :any,
